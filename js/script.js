593012599f4b6a49dbc5880e1bb402763bbcf14a
sashkatenev <tel256-32-79@yandex.ru>
2020-03-14 02:40:33 +0300

    Добавляет обработчик нажатия кнопок "В закладки"

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let buttonsBuy = document.querySelectorAll(".product-action-buy");
 let buttonCart = document.querySelector(".head-button-cart");
 
+let buttonsBookmark = document.querySelectorAll(".product-action-bookmark");
+let buttonBookmark = document.querySelector(".head-button-bookmark");
+
 let popupShoppingCart = document.querySelector(".popup-shopping-cart");
 
 let popupShoppingCartCloseButton = popupShoppingCart.querySelector(".popup-close-button");
@@ -16,3 +19,9 @@ popupShoppingCartCloseButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   popupShoppingCart.classList.remove("popup-show-flex");
 });
+
+for( let i = 0; i < buttonsBookmark.length; i++) {
+  buttonsBookmark[i].onclick = function() {
+    buttonBookmark.classList.add("not-empty");
+  }
+};
