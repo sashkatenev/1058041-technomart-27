2ad2c53926b794888b9b70b4b365e2aec5efe8a0
sashkatenev <60226999+sashkatenev@users.noreply.github.com>
2020-03-14 21:24:25 +0300

    Добавляет обработчик кнопки "Напишите нам" и ссылки обратной связи в подвале

--- js/script.js
+++ js/script.js
@@ -6,15 +6,19 @@ let buttonBookmark = document.querySelector(".head-button-bookmark");
 
 let userButtonLogin = document.querySelector(".user-button-log-in");
 let userButtonLogout = document.querySelector(".user-button-log-out");
+let buttonsWriteUs = document.querySelectorAll(".write-us");
 
 let popupShoppingCart = document.querySelector(".popup-shopping-cart");
+let popupFeedback = document.querySelector(".popup-feedback");
 
 let popupShoppingCartCloseButton = popupShoppingCart.querySelector(".popup-close-button");
 let continueShoppingButton = popupShoppingCart.querySelector("[id=continue_shopping]");
+let popupFeedbackCloseButton = null;
 
 let userNavigationBlocks = document.querySelectorAll(".user-navigation");
 let userNavigationHandler = null;
 
+// кнопки "Купить"
 for( let i = 0; i < buttonsBuy.length; i++) {
   buttonsBuy[i].onclick = function() {
     buttonCart.classList.add("not-empty");
@@ -22,22 +26,43 @@ for( let i = 0; i < buttonsBuy.length; i++) {
   }
 };
 
+// кнопка "Закрыть" окна "Товар добавлен"
 popupShoppingCartCloseButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   popupShoppingCart.classList.remove("popup-show-flex");
 });
 
+// кнопка "Продолжить покупки" окна "Товар добавлен"
 continueShoppingButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   popupShoppingCart.classList.remove("popup-show-flex");
 });
 
+// кнопка "Заблудились? Напишите нам" и ссылка обратной связи
+for(let i = 0; i < buttonsWriteUs.length; i++) {
+  buttonsWriteUs[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+      popupFeedback.classList.add("popup-show-flex");
+  });
+}
+
+// кнопка "Закрыть" формы обратной связи
+if(popupFeedback) {
+  popupFeedbackCloseButton = popupFeedback.querySelector(".popup-close-button");
+  popupFeedbackCloseButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popupFeedback.classList.remove("popup-show-flex");
+  });
+}
+
+// кнопки "В закладки"
 for( let i = 0; i < buttonsBookmark.length; i++) {
   buttonsBookmark[i].onclick = function() {
     buttonBookmark.classList.add("not-empty");
   }
 };
 
+// обработчик ссылок "Войти" и "Выход"
 userNavigationHandler = function(evt) {
   evt.preventDefault();
   for( let i = 0; i < userNavigationBlocks.length; i++) {
