d320213bbaf58e80482c99790630f44b946aa80f
sashkatenev <tel256-32-79@yandex.ru>
2020-03-14 03:13:32 +0300

    Добавляет обработчики события click ссылкам "Войти" и "Выход"

--- js/script.js
+++ js/script.js
@@ -4,9 +4,15 @@ let buttonCart = document.querySelector(".head-button-cart");
 let buttonsBookmark = document.querySelectorAll(".product-action-bookmark");
 let buttonBookmark = document.querySelector(".head-button-bookmark");
 
+let userButtonLogin = document.querySelector(".user-button-log-in");
+let userButtonLogout = document.querySelector(".user-button-log-out");
+
 let popupShoppingCart = document.querySelector(".popup-shopping-cart");
 
 let popupShoppingCartCloseButton = popupShoppingCart.querySelector(".popup-close-button");
+
+let userNavigationBlocks = document.querySelectorAll(".user-navigation");
+let userNavigationHandler = null;
 
 for( let i = 0; i < buttonsBuy.length; i++) {
   buttonsBuy[i].onclick = function() {
@@ -25,3 +31,13 @@ for( let i = 0; i < buttonsBookmark.length; i++) {
     buttonBookmark.classList.add("not-empty");
   }
 };
+
+userNavigationHandler = function(evt) {
+  evt.preventDefault();
+  for( let i = 0; i < userNavigationBlocks.length; i++) {
+    userNavigationBlocks[i].classList.toggle("user-navigation-hidden");
+  }
+};
+
+userButtonLogin.addEventListener("click", userNavigationHandler);
+userButtonLogout.addEventListener("click", userNavigationHandler);
